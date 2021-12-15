719e4ccd3d38600bc1f0463ff8256b7d67ca6d0b
Jayesh Prajapati <96169141+JustCoder123@users.noreply.github.com>
2021-12-15 13:15:35 +0530

    updated app.py

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
 	return 'Hello World'
+@app.route('/home')
+def hello_home():
+	return 'Welcome To Home Page'
 
 if __name__ == '__main__':
    app.run()
