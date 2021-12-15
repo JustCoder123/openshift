4ff566c39e2ccd80f1fb736ccc1d8c47594dab17
Jayesh Prajapati <96169141+JustCoder123@users.noreply.github.com>
2021-12-15 13:29:48 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
@@ -10,4 +11,4 @@ def hello_home():
 	return 'Welcome To Home Page'
 
 if __name__ == '__main__':
-   app.run()
+   app.run(host="0.0.0.0",port= 8080)
