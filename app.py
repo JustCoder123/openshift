513c017677c6d0fc231e7a9d5a893293c3d56631
Jayesh Prajapati <96169141+JustCoder123@users.noreply.github.com>
2021-12-15 16:48:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-	return 'Hello World'
+	return 'Hello in the World full of masti with jayesh '
 @app.route('/home')
 def hello_home():
-	return 'Welcome To Home Page'
+	return 'Welcome To Home Page of openshift appp '
 
 if __name__ == '__main__':
    app.run(host="0.0.0.0",port= 8080)
